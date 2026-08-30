687a1cf588a509b6d02007b713be6115f0724f12
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-08-30 14:54:47 +0100

    feat: adicionando numero na minha lista

--- identidade.java
+++
@@ -1,7 +0,0 @@
-import  java.util.ArrayList;
-public class testPessoa {
-
-    public static void main(String[] args) {
-        List<Intenger> number = new ArrayList<>();
-    }
-}
---
+++ testPessoa.java
@@ -0,0 +1,14 @@
+import  java.util.ArrayList;
+import java.util.List;
+public class testPessoa {
+
+    public static void main(String[] args) {
+        List<Integer> number = new ArrayList<>();
+
+
+          number.add(12);
+          number.add(14);
+          number.add(15);
+        
+    }
+}
